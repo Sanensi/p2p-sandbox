9ebfac84eee11fdac411a5028c2d02721c33500b
Sanensi <william.picard@hotmail.com>
2025-04-16 00:20:15 -0400

    feat: Can send p2p messages

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ type Offer = {
   iceCandidates: RTCIceCandidate[];
 };
 
-const { connection } = createConnection();
+const { connection, channel: initialChannel } = createConnection();
 
 await connection.setLocalDescription();
 const iceCandidates = await gatherICECandidates(connection);
 
 export function App() {
+  const [connectionState, setConnectionState] = useState(connection.connectionState);
   const [localOffer, setLocalOffer] = useState<Offer>({
     description: connection.localDescription,
     iceCandidates,
   });
   const [remoteOffer, setRemoteOffer] = useState<Offer>();
+
+  const [localMessages, setLocalMessages] = useState<string>("");
+  const [remoteMessages, setRemoteMessages] = useState<string>("");
+
+  useEffect(() => {
+    const updateConnectionState = (e: Event) => setConnectionState(e.target.connectionState);
+    connection.addEventListener("connectionstatechange", updateConnectionState);
+
+    return () => {
+      connection.removeEventListener("connectionstatechange", updateConnectionState);
+    };
+  }, []);
+
   const [channel, setChannel] = useState<RTCDataChannel>();
 
   useEffect(() => {
@@ -27,22 +41,10 @@ export function App() {
   }, []);
 
   useEffect(() => {
-    const logEvent = (e: Event) => console.log(e.type, e);
-
-    channel?.addEventListener("bufferedamountlow", logEvent);
-    channel?.addEventListener("close", logEvent);
-    channel?.addEventListener("closing", logEvent);
-    channel?.addEventListener("error", logEvent);
-    channel?.addEventListener("message", logEvent);
-    channel?.addEventListener("open", logEvent);
-
+    const updateRemoteMessages = (e: MessageEvent) => setRemoteMessages(e.data);
+    channel?.addEventListener("message", updateRemoteMessages);
     return () => {
-      channel?.removeEventListener("bufferedamountlow", logEvent);
-      channel?.removeEventListener("close", logEvent);
-      channel?.removeEventListener("closing", logEvent);
-      channel?.removeEventListener("error", logEvent);
-      channel?.removeEventListener("message", logEvent);
-      channel?.removeEventListener("open", logEvent);
+      channel?.removeEventListener("message", updateRemoteMessages);
     };
   }, [channel]);
 
@@ -58,54 +60,77 @@ export function App() {
       }}
     >
       <h1>P2P Sandbox</h1>
-      {!channel ? (
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "12px",
-          }}
-        >
-          <textarea
-            rows={10}
-            cols={120}
-            readOnly
-            style={{ resize: "none", textWrap: "nowrap" }}
-            value={JSON.stringify(localOffer, undefined, 2)}
-          />
-          <textarea
-            rows={10}
-            cols={120}
-            style={{ resize: "none", textWrap: "nowrap" }}
-            value={JSON.stringify(remoteOffer, undefined, 2)}
-            onChange={async (e) => {
-              try {
-                const remoteOffer: Offer = JSON.parse(e.target.value);
-                assert(remoteOffer);
-                assert(remoteOffer.description && typeof remoteOffer.description === "object");
-                assert(Array.isArray(remoteOffer.iceCandidates));
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "12px",
+          width: "820px",
+          maxWidth: "95%",
+        }}
+      >
+        <p>Connection state: {connectionState}</p>
 
-                await connection.setRemoteDescription(remoteOffer.description);
-                setRemoteOffer(remoteOffer);
+        {connectionState !== "connected" ? (
+          <>
+            <textarea
+              rows={10}
+              readOnly
+              style={{ resize: "none", textWrap: "nowrap" }}
+              value={JSON.stringify(localOffer, undefined, 2)}
+            />
+            <textarea
+              rows={10}
+              style={{ resize: "none", textWrap: "nowrap" }}
+              value={JSON.stringify(remoteOffer, undefined, 2)}
+              onChange={async (e) => {
+                try {
+                  const remoteOffer: Offer = JSON.parse(e.target.value);
+                  assert(remoteOffer);
+                  assert(remoteOffer.description && typeof remoteOffer.description === "object");
+                  assert(Array.isArray(remoteOffer.iceCandidates));
 
-                if (remoteOffer.description.type === "offer") {
-                  await connection.setLocalDescription();
-                  for (const candidate of remoteOffer.iceCandidates) {
-                    await connection.addIceCandidate(candidate);
+                  await connection.setRemoteDescription(remoteOffer.description);
+                  setRemoteOffer(remoteOffer);
+
+                  if (remoteOffer.description.type === "offer") {
+                    await connection.setLocalDescription();
+                    for (const candidate of remoteOffer.iceCandidates) {
+                      await connection.addIceCandidate(candidate);
+                    }
+                    const iceCandidates = await gatherICECandidates(connection);
+                    setLocalOffer({
+                      description: connection.localDescription,
+                      iceCandidates,
+                    });
                   }
-                  const iceCandidates = await gatherICECandidates(connection);
-                  setLocalOffer({
-                    description: connection.localDescription,
-                    iceCandidates,
-                  });
+                } catch (e) {
+                  console.error(e);
+                  setRemoteOffer(undefined);
                 }
-              } catch (e) {
-                console.error(e);
-              }
-            }}
-          />
-        </div>
-      ) : null}
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <textarea
+              rows={10}
+              readOnly
+              style={{ resize: "none", textWrap: "nowrap" }}
+              value={remoteMessages}
+            />
+            <textarea
+              rows={10}
+              style={{ resize: "none", textWrap: "nowrap" }}
+              value={localMessages}
+              onChange={(e) => {
+                setLocalMessages(e.target.value);
+                initialChannel.send(e.target.value);
+              }}
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 }
