b9b7b1ea95d5e3f3b9293bcc10a1fe61ad4612f1
Sanensi <william.picard@hotmail.com>
2025-04-18 18:24:29 -0400

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "p2p-sandbox",
+  base: "/p2p-sandbox",
   build: {
     target: "esnext",
   },
