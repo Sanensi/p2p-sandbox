e670d6f1de0c58fed1e623f0966f43f14e1cc8f2
Sanensi <william.picard@hotmail.com>
2025-04-19 01:31:11 -0400

    feat: Support non esnext browsers

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/p2p-sandbox",
-  build: {
-    target: "esnext",
-  },
   plugins: [react()],
 });
