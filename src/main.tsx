4c3487662676abe2d1efc6354668d26f71fd9343
Sanensi <william.picard@hotmail.com>
2025-04-21 14:22:10 -0400

    feat: Add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://19dedecafd5cc504a55324989fdc1b9b@o4509192617787392.ingest.us.sentry.io/4509192619753472",
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/p2p-sandbox",
-  plugins: [react()],
+
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
 });
